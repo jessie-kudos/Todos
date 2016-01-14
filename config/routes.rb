7848423209db6ccd883ae0b17507cf1b02f4587c
Jessie M. Klein <jmeredith.klein@gmail.com>
2016-01-14 16:17:42 -0700

    Set up Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :todos, except: :show do
     put :completed, on: :member
     delete :delete_all, on: :collection
   end
+  root to: "todos#index"
 end
