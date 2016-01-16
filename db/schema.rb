eb1f4c44c8a9c60dfe1d1311d0f182416d2ea800
Jessie M. Klein <jmeredith.klein@gmail.com>
2016-01-16 12:28:46 -0700

    Added one-to-many association between todos and user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114225811) do
+ActiveRecord::Schema.define(version: 20160116181005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160114225811) do
     t.boolean  "complete",   default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "user_id"
   end
+
+  add_index "todos", ["user_id"], name: "index_todos_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
